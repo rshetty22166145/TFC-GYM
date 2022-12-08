265488bad51e9cdc51d304ae735b075f2a1e31be
rshetty22166@gmail.com <rshetty22166@gmail.com>
2022-12-08 00:59:19 -0500

    Register with frontend Validation and CSS added

--- PF/frontend/src/App.js
+++ PF/frontend/src/App.js
@@ -3,12 +3,16 @@ import StudioDetail from "./routes/StudioDetail";
 import StudioList from "./routes/StudioList";
 import Home from "./routes/Home";
 import Login from './Login';
+import Register from './Register';
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/login">
           <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
         </Route>
         <Route path="/studio/:id">
           <StudioDetail />
