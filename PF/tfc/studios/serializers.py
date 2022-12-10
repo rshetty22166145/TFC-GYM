9e366939528486f3fec29f47f69a151c7b4618e2
Sung Hyun Cho <roy.cho@mail.utoronto.ca>
2022-12-10 01:06:34 -0500

    Update serializers.py

--- PF/tfc/studios/serializers.py
+++ PF/tfc/studios/serializers.py
@@ -2,6 +2,7 @@ import json
 
 from rest_framework import serializers
 
+from classes.models import Class
 from studios.models import Studio, Image
 
 
@@ -15,10 +16,30 @@ class ImageSerializer(serializers.ModelSerializer):
 class GeoLocationSerializer(serializers.ModelSerializer):
     imagesSerializer = ImageSerializer(many=True, source='images')
 
+    amenities = serializers.SerializerMethodField()
     geographical_location = serializers.SerializerMethodField()
+    classes = serializers.SerializerMethodField()
 
     def get_geographical_location(self, obj):
         return json.loads(obj.geographical_location)
+
+    def get_amenities(self, obj):
+        lst = []
+        print(obj.amenities.all())
+        for amenity in obj.amenities.all():
+            lst.append(amenity.type)
+        return lst
+
+    def get_classes(self, obj):
+        lst = []
+        print(obj.classes.all())
+        print(obj)
+        classes = Class.objects.filter(studio=obj)
+        print(classes)
+        for cla in obj.classes.all():
+            dictionary = {"id": cla.id, "name": cla.name, "description": cla.description}
+            lst.append(dictionary)
+        return lst
 
     class Meta:
         model = Studio
